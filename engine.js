979a1eec14c6790a3907c4a74684f791267a6cda
bhoang8855 <bhoang8855@sbcglobal.net>
2017-06-04 19:41:06 -0700

    Added background music and deleted index.html file because there is already a working App.html file.

--- engine.js
+++ engine.js
@@ -1,117 +1,127 @@
-/* Engine.js
- * Updates the entities and renders the game
+/**
+ * Arian Cuellar & Brian Hoang
+ * CS408 Group Project
+ * Created a Frogger esque game in Javascript
  */
 
-var Engine = (function(global) {
-    var doc = global.document,
-        win = global.window,
-        canvas = doc.createElement('canvas'),
-        ctx = canvas.getContext('2d'),
-        lastTime;
-    canvas.width = 1000;
-    canvas.height = 1000;
-    doc.body.appendChild(canvas);
+/* Engine.js
+ * Updates the entities,loads the images, and renders the game
+ */
 
-    function main() {
-        // Get time delta infomration for smooth animation
-        var now = Date.now(),
-            dt = (now - lastTime) / 1000.0;
-        /* Call update/render functions and pass along the time delta to
-         * the update function for smooth animation.
-         */
-        update(dt);
-        render();
-        /* Set lastTime variable which is used to determine the time delta
-         * for the next time this function is called.
-         */
-        lastTime = now;
-        /* Use browser's requestAnimationFrame function to call this
-         * function again as soon as the browser is able to draw another frame.
-         */
-        win.requestAnimationFrame(main);
-    };
+var imgCache = {};
+var canvas = document.createElement('canvas');
+var ctx = canvas.getContext('2d');
+var then;
+canvas.width = 1000;
+canvas.height = 1000;
+document.body.appendChild(canvas);
+// Load all images for the game.
+// Once images are loaded game will start.
+loadStart(['Images/groundBlock.jpg', 'Images/goalBlock.png', 'Images/blockBlock.png', 'Images/mario.png', 'Images/koopa.png', 'Images/goomba.png'],start);
 
-    function init() {
-        lastTime = Date.now();
-        main();
-    }
-
-    // Calls functions to update entity's data.
-    function update(dt) {
-        updateEntities(dt);
-        checkCollisions();
-    }
-
-    // Calls all enemies update() methods and player's update method.
-    function updateEntities(dt) {
-        allEnemies.forEach(function(enemy) {
-            enemy.update(dt);
-        });
-        if ( Math.round(player.y / 80) === 0 ) {
-            player.update();
-        }
-        doc.getElementById('score').innerHTML = player.score;
-    }
-    // Checks for collisions between player and enemies
-    function checkCollisions() {
-        allEnemies.forEach(function(enemy) {
-            if ( Math.round(enemy.x / 100) === Math.round(player.x / 100) &&
-                 Math.round(enemy.y / 100) === Math.round(player.y / 100) ) {
-                    player.reset();
+//Image Loader
+//Takes the array containing strings pointing to image locations and loads them
+function loadStart(arr,func) {
+    arr.forEach(function(location) {
+        if(imgCache[location])
+            return imgCache[location];
+        var image = new Image();
+        image.onload = function() {
+            imgCache[location] = image;
+            var ready = true;
+            for(var i in imgCache) {
+                if(!imgCache[i]) {
+                    ready = false;
+                    break;
+                }
             }
-        });
-    }
-
-    // Renders the initial game setup
-    function render() {
-        /* This array holds the relative URL to the image used
-         * for that particular row of the game level.
-         */
-        var rowImages = [
-                'Images/goalBlock.png',   // Top row is water
-                'Images/blockBlock.png',
-                'Images/groundBlock.jpg',
-                'Images/blockBlock.png',
-                'Images/groundBlock.jpg',
-                'Images/blockBlock.png',
-                'Images/groundBlock.jpg',
-                'Images/blockBlock.png',
-                'Images/groundBlock.jpg'
-            ],
-            numRows = 9,
-            numCols = 8,
-            row, col;
-        /* Loop through the number of rows and columns defined above
-         * and, using the rowImages array, draw the correct image for that
-         * portion of the grid
-         */
-        for (row = 0; row < numRows; row++) {
-            for (col = 0; col < numCols; col++) {
-                ctx.drawImage(Resources.get(rowImages[row]), col * 100, row * 100);
+            //If all images are loaded, launches the parameter function
+            if(ready) {
+                func();
             }
-        }
-        renderEntities();
-    }
+        };
+        imgCache[location] = false;
+        image.src = location;
+    });
+}
 
-    // calls the render functions of enemies and players on each game tick
-    function renderEntities() {
-        allEnemies.forEach(function(enemy) {
-            enemy.render();
-        });
-        player.render();
+//Grabs images that have already been cached
+function getImage(location) {
+    return imgCache[location];
+}
+// Game loop that will continually update and render the game.
+function main() {
+    // Get time delta information for smooth animation
+    var now = Date.now(),
+        delta = (now - then) / 1000.0;
+    /* Call update/render functions and pass along the time delta to
+       * the update function for smooth animation.
+        */
+    updateAll(delta);
+    checkCollisions();
+    renderAll();
+    /* Set then variable which is used to determine the time delta
+     * for the next time this function is called.
+     */
+    then = now;
+    /* Use browser's requestAnimationFrame function to call this
+     * function again as soon as the browser is able to draw another frame.
+     */
+    window.requestAnimationFrame(main);
+}
+/**
+ * Function that starts the actual game after all images have been loaded properly
+ */
+function start() {
+    then = Date.now();
+    main();
+}
+// Calls all enemies update() methods and player's update method.
+function updateAll(delta) {
+    allEnemies.forEach(function (enemy) {
+        enemy.update(delta);
+    });
+    if (Math.round(player.y / 95) === 0) {
+        player.update();
     }
-
-    // Load all images for the game.
-    // Once images are loaded game will start.
-    Resources.load([
-        'Images/groundBlock.jpg',
-        'Images/goalBlock.png',
+    document.getElementById('score').innerHTML = player.score;
+}
+// Renders the initial game setup
+function renderAll() {
+    /* This array holds the relative URL to the image used
+        * for that particular row of the game level.
+       */
+    var images = ['Images/goalBlock.png',
         'Images/blockBlock.png',
-        'Images/mario.png',
-        'Images/koopa.png',
-        'Images/goomba.png'
-
-    ]);
-    Resources.onReady(init);
-    global.ctx = ctx;
-})(this);
+        'Images/groundBlock.jpg',
+        'Images/blockBlock.png',
+        'Images/groundBlock.jpg',
+        'Images/blockBlock.png',
+        'Images/groundBlock.jpg',
+        'Images/blockBlock.png',
+        'Images/groundBlock.jpg'];
+    var row;
+    var col;
+    /* Loop through the number of rows and columns defined above
+        * and, using the images array, draw the correct image for that
+        * portion of the grid
+        */
+    for (row = 0; row < 9; row++) {
+        for (col = 0; col < 8; col++) {
+            ctx.drawImage(getImage(images[row]), col * 100, row * 100);
+        }
+    }
+    /**
+     * Renders all enemies and the player
+     */
+    allEnemies.forEach(function(enemy) {
+        enemy.render();
+    });
+    player.render();
+}
+myAudio = new Audio('Sounds/super-mario-bros.mp3');
+myAudio.addEventListener('ended', function() {
+    this.currentTime = 0;
+    this.play();
+}, false);
+myAudio.play();
